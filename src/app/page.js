d1eda10732647f4403730749aff39d44ff39f230
kopsus <emonsetio01@gmail.com>
2023-12-15 17:40:17 +0700

    [delete] role in cookies

--- src/app/page.js
+++ src/app/page.js
@@ -5,40 +5,15 @@ import SectionHome from "./SectionHome"
 import Edukasi from "@/components/Carousel/CarouselEdukasi"
 import Navbar from "@/components/Navbar/Navbar"
 import Footer from "@/components/Footer/Footer"
-import { useEffect, useState } from "react"
-import SideBar from "@/components/Docter/SideBar/SideBar"
-import Dashboard from "@/components/Docter/Dashboard/Dashboard"
-import Loading from "./loading"
-import Cookies from "js-cookie"
 
 export default function Home() {
-  const [userLogin, setUserLogin] = useState("")
-
-  useEffect(() => {
-    const roleLogin = Cookies.get("role")
-    setUserLogin(roleLogin)
-  }, [])
-
-  if (userLogin === "") {
-    return <Loading />
-  }
-
-  if (userLogin === "dokter") {
-    return (
-      <>
-        <SideBar />
-        <Dashboard />
-      </>
-    )
-  } else {
-    return (
-      <>
-        <Navbar />
-        <WrapperHome />
-        <SectionHome />
-        <Edukasi />
-        <Footer />
-      </>
-    )
-  }
+  return (
+    <>
+      <Navbar />
+      <WrapperHome />
+      <SectionHome />
+      <Edukasi />
+      <Footer />
+    </>
+  )
 }
